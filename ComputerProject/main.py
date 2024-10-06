c9ef3b0b2f78f0c146a68bd006b8a1f70c4a8472
Shreyas Parthaje <84122024+Shreyas-Parthaje@users.noreply.github.com>
2024-10-06 16:54:50 +0530

    Add files via upload

--- ComputerProject/main.py
+++ ComputerProject/main.py
@@ -1,18 +1,16 @@
-import os
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTextEdit, QLabel, QFrame, QPushButton, QLineEdit, QMessageBox, QGraphicsBlurEffect, QGraphicsDropShadowEffect
+from PyQt5.QtWidgets import QMainWindow, QApplication, QCalendarWidget, QTextEdit, QLabel, QFrame, QPushButton, QLineEdit, QMessageBox, QGraphicsBlurEffect, QGraphicsDropShadowEffect
 from PyQt5 import uic
 import sys
 import backgroundimg
 import background_addEarningAndExpenseWindow
 from dbFunctions import *
 
-
-
 #QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True) #enable highdpi scaling
 #QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
 uid=0
+calenderWindowSource=0
 # Code for login window
 class LoginWindow(QMainWindow):
     def __init__(self):
@@ -110,23 +108,53 @@ class RegisterWindow(QMainWindow):
             msg.setText("User already exists.")
             msg.exec_()
 
+class deleteRecordWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        uic.loadUi('UI/deleteRecordWindow.ui', self)
+
+        
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        global uid
+
+        self.connection = DatabaseConnection().get_connection()
 
         #Loading .ui file
         uic.loadUi('UI/mainwindow.ui', self)
 
         self.addEarningButton=self.findChild(QPushButton, "pushButton")
         self.lightThemeCheckBox=self.findChild(QPushButton, "pushButton_3")
+        self.totalSpendingValue=self.findChild(QLabel, "label_7")
+        self.totalEarningValue=self.findChild(QLabel, "label_5")
+        self.searchButton=self.findChild(QPushButton, "pushButton_4")
+        self.deleteRecordButton=self.findChild(QPushButton, "pushButton_5")
+
 
         self.addEarningButton.clicked.connect(self.openAddEarningWindow)
         self.lightThemeCheckBox.clicked.connect(self.switchToLightTheme)
+        self.searchButton.clicked.connect(self.openSearchResults)
+        self.deleteRecordButton.clicked.connect(self.openDeleteRecordPopup)
+        
+        self.recordResult=view_recent_records(self.connection, uid)
+        
+        self.totalEarningValue.setText(str(self.recordResult[0]))
+        self.totalSpendingValue.setText(str(self.recordResult[1]))
+
+    def openDeleteRecordPopup(self):
+        self.w=deleteRecordWindow()
+        self.w.show()
+
 
     def openAddEarningWindow(self):
         self.w=addEarningWindow()
+        self.w.show()
+
+    def openSearchResults(self):
+        self.w=searchResult()
         self.w.show()
 
     def switchToLightTheme(self):
@@ -135,6 +163,40 @@ class MainWindow(QMainWindow):
         
         else:
             self.lightThemeCheckBox.setStyleSheet('QPushButton{background-color:rgb(61, 40, 224);border:2px solid;border-radius:7px;border-color:rgb(62, 58, 83);}')
+
+class searchResult(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi('searchResult.ui', self)
+
+        
+
+
+class calenderWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        global date
+        uic.loadUi('UI/calenderWidget.ui', self)
+
+        self.calender=self.findChild(QCalendarWidget, 'calendarWidget')
+        self.setDateButton=self.findChild(QPushButton, 'pushButton')
+
+        self.setDateButton.clicked.connect(self.setDate)
+
+    def setDate(self):
+        global calenderWindowSource
+        self.selectedDate=self.calender.selectedDate().toString()
+
+        if calenderWindowSource=='addEarningWindow':
+            self.addEarningWindow=addEarningWindow()
+            self.addEarningWindow.dateButton.setText(self.selectedDate)
+            self.addEarningWindow.show()
+        else:
+            self.addExpenseWindow=addExpenseWindow()
+            self.addExpenseWindow.dateButton.setText(self.selectedDate)
+            self.addExpenseWindow.show()
 
 class addExpenseWindow(QMainWindow):
     def __init__(self):
@@ -153,17 +215,26 @@ class addExpenseWindow(QMainWindow):
         self.dateButton=self.findChild(QPushButton, "pushButton_2")
         self.descriptionField=self.findChild(QTextEdit, "textEdit")
 
-
         self.openEarningWindowButton.clicked.connect(self.openEarningWindow)
         self.recurringExpenseCheckBox.clicked.connect(self.changeState)
         self.addExpenseButton.clicked.connect(self.addExpense)
+        self.dateButton.clicked.connect(self.openCalenderWindow)
 
+        #This code has to be improved.
         self.dateText=self.dateButton.text().split('/')
         self.sqlDateText=''
 
         for i in self.dateText[::-1]:
             self.sqlDateText+=i+'-'
         self.sqlDateText=self.sqlDateText[0:-1]
+
+    def openCalenderWindow(self):
+        global calenderWindowSource
+
+        calenderWindowSource='addExpenseWindow'
+        self.close()
+        self.w=calenderWidget()
+        self.w.show()
         
     def openEarningWindow(self):
         self.w=addEarningWindow()
@@ -209,13 +280,23 @@ class addEarningWindow(QMainWindow):
         self.openExpenseWindowButton.clicked.connect(self.openExpenseWindow)
         self.recurringEarningCheckBox.clicked.connect(self.changeState)
         self.addEarningButton.clicked.connect(self.addEarning)
+        self.dateButton.clicked.connect(self.openCalenderWindow)
 
+        #This code has to be improved
         self.dateText=self.dateButton.text().split('/')
         self.sqlDateText=''
 
         for i in self.dateText[::-1]:
             self.sqlDateText+=i+'-'
         self.sqlDateText=self.sqlDateText[0:-1]
+
+    def openCalenderWindow(self):
+        global calenderWindowSource
+
+        calenderWindowSource='addEarningWindow'
+        self.close()
+        self.w=calenderWidget()
+        self.w.show()
 
 
     def addEarning(self):
